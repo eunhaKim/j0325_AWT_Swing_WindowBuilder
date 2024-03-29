1a90ad959f875702f30987bb0551d9ee6466be0e
eunhaKim <ara419@naver.com>
2024-03-29 09:25:50 +0900

    수정본

--- src/t7_windowBuilder/T8_File.java
+++ src/t7_windowBuilder/T8_File.java
@@ -1,28 +1,28 @@
 package t7_windowBuilder;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Vector;
+
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-
-import java.awt.Font;
-import java.awt.ScrollPane;
-
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
-
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.Vector;
-import java.awt.event.ActionEvent;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 
 @SuppressWarnings("serial")
 public class T8_File extends JFrame {
@@ -100,8 +100,52 @@ public class T8_File extends JFrame {
 		
 // =============================================================================
 		setVisible(true);
-// =============================================================================
+// =====================아래로 메소드처리영역========================================================
 		
+		// JTable에 등록된 그림을 선택후 그림 삭제처리...
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int row = table.getSelectedRow();
+				int col = table.getSelectedColumn();
+				String image = table.getValueAt(row, col).toString();
+				//System.out.println("image : " + image);
+				if(image != null) {
+					int ans = JOptionPane.showConfirmDialog(null, "다음파일을 삭제하시겠습니까?\n"+image,"삭제화면", JOptionPane.YES_NO_OPTION);
+					if(ans == 0) {
+						File file = new File(image);
+						file.delete();
+						JOptionPane.showMessageDialog(null, "파일을 삭제처리했습니다.");
+						lblImage.setIcon(null);
+						getFolderInfor();
+						dtm.setDataVector(vData, title);
+					}
+					else JOptionPane.showMessageDialog(null, "삭제취소!!");
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "파일을 선택해 주세요.");
+				}
+			}
+		});
+		
+		// Jtable 클릭시 그림파일의 정보를 레이블에 뿌려주기(그림 보여주기)
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int row = table.getSelectedRow();
+				int col = table.getSelectedColumn();
+				String image = table.getValueAt(row, col).toString();
+				lblImage.setIcon(new ImageIcon(image));
+				
+				ImageIcon icon = new ImageIcon(image);
+				Image img = icon.getImage();
+				Image updateImg = img.getScaledInstance(349, 234, Image.SCALE_SMOOTH);
+				
+				ImageIcon updateIcon = new ImageIcon(updateImg);	// 변경된 크기의 이미지로 다시 생성..
+				
+				lblImage.setIcon(updateIcon);		// 생성된 이미지를 레이블에 아이콘으로 등록후 뿌려준다.(setText())
+				lblImage.setText(image);
+			}
+		});
 		
 		// 그림 업로드 처리
 		btnUpload.addActionListener(new ActionListener() {
@@ -136,8 +180,11 @@ public class T8_File extends JFrame {
 						if(!file.exists()) file.mkdir();	// 폴더가 존재하지않으면 폴더를 만들어준다.
 						
 						ImageIO.write(image, extension, file);	// images를 file로 업로드시켜준다.
-						System.out.println("파일명 : " + fileName);
+						//System.out.println("파일명 : " + fileName);
 						JOptionPane.showMessageDialog(null, "이미지 업로드 완료!!");
+					
+						getFolderInfor();	// 폴더안의 정보를 가져오기
+						dtm.setDataVector(vData, title);
 					} catch (Exception e2) {
 						e2.printStackTrace();
 					}
@@ -166,12 +213,12 @@ public class T8_File extends JFrame {
 		vData = new Vector<>();
 		if(files.length != 0) {
 			for(int i=0; i<files.length; i++) {
-				System.out.println("파일("+(i+1)+") : " + files[i]);
+				//System.out.println("파일("+(i+1)+") : " + files[i]);
 				Vector vo = new Vector<>();
 				vo.add(files[i]);
 				vData.add(vo);
 			}
-			dtm.setDataVector(vData, title);
+			//dtm.setDataVector(vData, title);
 		}
 	}
 
